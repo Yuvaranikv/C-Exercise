9b7939430847c5475e7cac1cb46625cbfba174a1
yuvarani <yuvarani@WSAMZN-58KUA3BN>
2024-07-17 16:58:27 +0530

    Add project files.

---
+++ HellloFrom Csharp/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+        Console.WriteLine("Enter ur name : ");
+string name = Console.ReadLine();
+
+Console.WriteLine("hello"+name);    
